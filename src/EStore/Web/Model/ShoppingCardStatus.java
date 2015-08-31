0906d8cbb840732492b0b060a8bdc40b34025831
root <yan.jiwenyi.jiwen@gmail.com>
2015-08-31 23:29:01 +0200

    shopping card status initialized

--- src/EStore/Web/Model/ShoppingCardStatus.java
+++ src/EStore/Web/Model/ShoppingCardStatus.java
@@ -1,13 +1,25 @@
 package EStore.Web.Model;
 
 public enum ShoppingCardStatus {
-	unknown,
-	initialized,
-	pending,
-	paied,
-	completed,
-	refunded,
-	closed,
-	onError
+	unknown(1),
+	initialized(2),
+	pending(3),
+	paied(4),
+	confirmed(5),
+	completed(6),
+	refunded(7),
+	closed(8),
+	onError(9);
+	
+	
+	private int code;
+	
+	private ShoppingCardStatus(int code){
+		this.code = code;
+	}
+	
+	public int getCode(){
+		return this.code;
+	}
 
 }
