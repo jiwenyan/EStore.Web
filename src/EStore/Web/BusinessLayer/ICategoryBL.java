60af8c7f94360deb019b30ffc8a7c41bf916eb8d
root <yan.jiwenyi.jiwen@gmail.com>
2015-09-10 23:13:46 +0200

    CategoryBL declared

--- src/EStore/Web/BusinessLayer/ICategoryBL.java
+++ src/EStore/Web/BusinessLayer/ICategoryBL.java
@@ -1,5 +1,13 @@
 package EStore.Web.BusinessLayer;
 
-public interface ICategoryBL {
+import java.util.List;
 
+import EStore.Web.Model.Category;
+import EStore.Web.Model.Product;
+
+public interface ICategoryBL {
+	public List<Category> getAllCategories();
+	public void createNewCategory(Category toCreate);
+	public void addProductToCategory(Category category,Product toAdd);
+	public List<Product> getProductByCategory(Category category);
 }
